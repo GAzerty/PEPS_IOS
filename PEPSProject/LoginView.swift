27fbd0b6a818efef6f122498a09b47c85751501d
paolaANDREU <paola.andreu@etu.umontpellier.fr>
2020-02-25 15:20:54 +0100

    test

---
+++ PEPSProject/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  PEPSProject
+//
+//  Created by Paola Andreu on 25/02/2020.
+//  Copyright © 2020 user164567. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
