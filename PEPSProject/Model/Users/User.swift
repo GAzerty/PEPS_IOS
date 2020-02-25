7413c0326efb78a4aeec32dc2312cf406386f6b0
user164567 <user164567@92-63-141-247.rdns.melbourne.co.uk>
2020-02-25 22:17:13 +0000

    getUserById() added

--- PEPSProject/Model/Users/User.swift
+++ PEPSProject/Model/Users/User.swift
@@ -10,10 +10,48 @@ import Foundation
 
 class User{
     
-    var idUser: Int
-    var pseudo: String
-    var password: String
-    var role: String
+    var idUser: Int?
+    var pseudo: String?
+    var password: String?
+    var role: String?
+    var token: String?
+    
+    func getById(idUser: Int){
+        if let urlAPI = URL(string: "https://web-ios-api.herokuapp.com/users/"+String(idUser)){
+             print(urlAPI)
+        
+             URLSession.shared.dataTask(with: urlAPI) { data, response, error in
+                 if let data = data {
+                     
+                     if let jsonObj = try? JSONSerialization.jsonObject(with: data, options: [JSONSerialization.ReadingOptions.mutableLeaves]){
+                         //print(jsonObj)
+                         
+                         if let dictionnaryData = jsonObj as? [String:Any]{
+                             print(dictionnaryData)
+                             //print(dictionnaryData["data"] as! [Any])
+                             
+                             //On recupere "data" comme un tableau de type Any
+                             if let dataT = dictionnaryData["data"] as? [Any]{
+                                 for user in dataT{
+                                     if let newUser = user as? [String:Any]{
+                                         //print(newUser)
+                                        self.idUser = (newUser["idUser"] as! Int)
+                                        self.password = (newUser["password"] as! String)
+                                        self.role = (newUser["role"] as! String)
+                                        self.pseudo = (newUser["pseudo"] as! String)
+                                     }
+                                 }
+                             }
+                         }
+                     }
+                 }
+             }.resume()
+         }
+    }
+    
+    init(idUser: Int){
+        getById(idUser: idUser)
+    }
     
     init(idUser: Int,pseudo: String, password: String, role: String){
         self.idUser = idUser
