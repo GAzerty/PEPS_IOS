7eef924dda11c33475c97695edcbf21727d1789d
paolaANDREU <paola.andreu@etu.umontpellier.fr>
2020-02-27 17:42:12 +0100

    modif remarkRowView

--- PEPSProject/RemarksViews/RemarkRowView.swift
+++ PEPSProject/RemarksViews/RemarkRowView.swift
@@ -13,21 +13,38 @@ struct RemarkRowView: View {
     var remark: Remark!
     
     var body: some View {
-        VStack{
-            HStack{
-                Text("\(remark.user.pseudo!)")
-                Text("DATE 01/01/2020")
+      VStack{
+        HStack{
+            VStack(alignment: .leading){
+                HStack{
+                    Image(systemName: "person")
+                    Text("\(remark.user.pseudo ?? "pseudo")")
+                    Spacer()
+                    Text("le remark.date")
+                }
+                HStack{
+                    Image(systemName: "mappin")
+                    Text("à remark.location")
+                }
             }
+            
+               
+        }.padding(10)
+        Divider()
+        Text("\(remark.remark)").padding(10)
+        Divider()
+        HStack{
+            Text("remark.nbComments  comments")
+            Spacer()
             HStack{
-                Text("\(remark.remark)")
+                Text("remark.nbEars")
+                Image(systemName: "volume")
             }
-            HStack{
-                Text("NB COMMENTS")
-                Text("ENCOUNTER NUMBER")
-            }
-        }
+        }.padding(10.0)
+    }.background(Color(UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1.0))).cornerRadius(30).padding(10)
     }
 }
+
 
 struct RemarkRowView_Previews: PreviewProvider {
     static var previews: some View {
