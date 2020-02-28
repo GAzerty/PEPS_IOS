5bb6b409d88374a0274d95ae32b353c53a9a652c
paolaANDREU <paola.andreu@etu.umontpellier.fr>
2020-02-28 14:01:35 +0100

    researchBar View

---
+++ PEPSProject/RemarksViews/ResearchBarView.swift
@@ -0,0 +1,46 @@
+//
+//  ResearchBarView.swift
+//  PEPSProject
+//
+//  Created by Paola Andreu on 27/02/2020.
+//  Copyright © 2020 user164567. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResearchBarView: View {
+    @State private var searchTerm : String = ""
+    @State private var showCancelButton: Bool = false
+      
+    @State private var showDetail = false
+      
+    var body: some View {
+        HStack(alignment: .top){
+            HStack{
+                Image(systemName: "magnifyingglass")
+                TextField("search", text: $searchTerm, onEditingChanged: { isEditing in
+                     self.showCancelButton = true
+                 }, onCommit: {
+                     print("onCommit")
+                 }).foregroundColor(.primary)
+
+                Button(action: {
+                     self.searchTerm = ""
+                 }) {
+                    Image(systemName: "xmark.circle.fill").opacity(searchTerm == "" ? 0 : 1)
+                 }
+              
+             }
+             .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+             .foregroundColor(.secondary)
+             .background(Color(.secondarySystemBackground))
+             .cornerRadius(10.0)
+        }
+    }
+}
+
+struct ResearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResearchBarView()
+    }
+}
