476faa66f10b7ad981a98f89ff009c6f3521e298
paolaANDREU <paola.andreu@etu.umontpellier.fr>
2020-03-03 14:50:16 +0100

    modif filters

--- PEPSProject/RemarksViews/FiltersView.swift
+++ PEPSProject/RemarksViews/FiltersView.swift
@@ -27,10 +27,13 @@ struct FiltersView: View {
                             self.arePressed.append(i)
                             self.categoriesSelected.append(self.categorySet.categorySet[i].idCategory)
                         }else{
+                            print("supprimer")
                             self.arePressed.remove(at: self.arePressed.firstIndex(of: i)!)
+                            self.categoriesSelected.remove(at: self.categoriesSelected.firstIndex(of: self.categorySet.categorySet[i].idCategory)!)
                             
                         }
-                        
+                        print(i)
+                        print(self.categoriesSelected)
                         self.remarkSetSelected.remarkSet = self.remarkSetBase.filterBy(idCategories: self.categoriesSelected)
                              }) {
                                 Text("\(self.categorySet.categorySet[i].lib)").padding(5).foregroundColor(.secondary).background(self.arePressed.contains(i) ? Color.gray : Color(.secondarySystemBackground)).cornerRadius(50)
