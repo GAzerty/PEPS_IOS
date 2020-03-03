74ef38efe386fed9feedf33185915273862c8600
user164567 <user164567@92-63-141-247.rdns.melbourne.co.uk>
2020-03-03 12:00:01 +0000

    Check of response status changed

--- PEPSProject/QueryService/QueryService.swift
+++ PEPSProject/QueryService/QueryService.swift
@@ -14,11 +14,11 @@ class QueryService {
     //httpMethod: String (GET,POST,PUT,DELETE)
     //httpBody: Data? (JSON data to send or nil if it's not required)
     func request(url: String, httpMethod: String, httpBody: Data?) -> [String:Any]?{
-
+        
         var result : [String:Any]?
         print(url)
         if let urlAPI = URL(string: url){
-                       
+            
             var request : URLRequest = URLRequest(url: urlAPI)
             request.httpMethod = httpMethod
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -40,26 +40,26 @@ class QueryService {
                 //Verification du Status Code
                 if !(200...299).contains(httpResponse.statusCode) {
                     print("Response Status Code: "+String(httpResponse.statusCode))
-                }else{
-                    
-                    //Verification des donnees
-                    guard let data = data else {
-                        print("Data receive not conform")
-                        return
-                    }
-                    
-                    //Serialisation en JSON des donnees reçues
-                    guard let jsonObj = try? JSONSerialization.jsonObject(with: data, options: [JSONSerialization.ReadingOptions.mutableLeaves]) else {
-                        print("JSON Serialization error")
-                        return
-                    }
-                    
-                    if let dictionnaryData = jsonObj as? [String:Any]{
-                        //print(dictionnaryData)
-                        result = dictionnaryData
-                    }
-                    group.leave()
                 }
+                
+                //Verification des donnees
+                guard let data = data else {
+                    print("Data receive not conform")
+                    return
+                }
+                
+                //Serialisation en JSON des donnees reçues
+                guard let jsonObj = try? JSONSerialization.jsonObject(with: data, options: [JSONSerialization.ReadingOptions.mutableLeaves]) else {
+                    print("JSON Serialization error")
+                    return
+                }
+                
+                if let dictionnaryData = jsonObj as? [String:Any]{
+                    //print(dictionnaryData)
+                    result = dictionnaryData
+                }
+                group.leave()
+                
             }.resume()
             group.wait()
         }
