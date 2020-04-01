428b85715be252159188d1191c0c0e04c81d7fab
user164567 <user164567@92-63-141-247.rdns.melbourne.co.uk>
2020-04-01 10:38:27 +0100

    Category picker solved in Form for create/update a remark/answer

--- PEPSProject/AnswersViews/CreateAnswerView.swift
+++ PEPSProject/AnswersViews/CreateAnswerView.swift
@@ -20,73 +20,90 @@ struct CreateAnswerView: View {
     var answerUpdated : Answer?
     
     var body: some View {
-        VStack{
+        
+        NavigationView{
             
-            if(!self.isUpdateView){
-                Text("Create an Answser").font(.title).foregroundColor(.purple).cornerRadius(10.0).padding()
-            }else{
-                Text("Update a Answser").font(.title).foregroundColor(.purple).cornerRadius(10.0).padding()
-            }
-            TextField("Answser...", text: self.$answer).padding().background(Color(.secondarySystemBackground)).cornerRadius(10)
-            Picker(selection: $selectedidCategory, label: Text("Category")){
-                ForEach(0 ..< categorySet.categorySet.count) {
-                    Text("\(self.categorySet.categorySet[$0].lib)").padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground)).cornerRadius(10)
+            VStack{
+                Form{
+                    Section{
+                        
+                        VStack{
+                            
+                            if(!self.isUpdateView){
+                                Text("Create an Answser").font(.title).foregroundColor(.purple).cornerRadius(10.0).padding()
+                            }else{
+                                Text("Update a Answser").font(.title).foregroundColor(.purple).cornerRadius(10.0).padding()
+                            }
+                            TextField("Answser...", text: self.$answer).padding().background(Color(.secondarySystemBackground)).cornerRadius(10)
+                            Picker(selection: $selectedidCategory, label: Text("Category")){
+                                ForEach(0 ..< categorySet.categorySet.count) {
+                                    Text("\(self.categorySet.categorySet[$0].lib)").padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground)).cornerRadius(10)
+                                }
+                            }.padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground))
+                                .cornerRadius(10)
+                            
+                        }
+                        
+                    }
+                    
                 }
-            }.padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground))
-            .cornerRadius(10)
-            
-            Button(action:{
-                if self.answer != ""{
-                    
-                    
-                    let idCategory = self.categorySet.categorySet[self.selectedidCategory].idCategory
-                    
-                    if(!self.isUpdateView){
-                        if let idAnswerCreated = AnswerQueryService().createAnswser(answser: self.answer, idCategory: idCategory){
-                            if let remark = self.remark{
-                                if RemarkQueryService().linkAnswserToRemark(idRemark: remark.idRemark, idAnswer: idAnswerCreated){
+                VStack{
+                    Button(action:{
+                        if self.answer != ""{
+                            
+                            
+                            let idCategory = self.categorySet.categorySet[self.selectedidCategory].idCategory
+                            
+                            if(!self.isUpdateView){
+                                if let idAnswerCreated = AnswerQueryService().createAnswser(answser: self.answer, idCategory: idCategory){
+                                    if let remark = self.remark{
+                                        if RemarkQueryService().linkAnswserToRemark(idRemark: remark.idRemark, idAnswer: idAnswerCreated){
+                                            
+                                            guard let user = UserQueryService().getUserLogged() else{
+                                                return
+                                            }
+                                            guard let nbLike = AnswerQueryService().getNbLikeAnswer(idAnswer: idAnswerCreated) else{
+                                                return
+                                            }
+                                            let newAnswer = Answer(idAnswer: idAnswerCreated, answer: self.answer, category: idCategory, user: user,nbLike: nbLike)
+                                            remark.answerSet.addAnswers(answer: newAnswer)
+                                            self.isPresented.toggle()
+                                        }
+                                    }
                                     
-                                    guard let user = UserQueryService().getUserLogged() else{
-                                     return
+                                }
+                            }else{
+                                
+                                if let answerUpdated = self.answerUpdated{
+                                    if AnswerQueryService().updateAnswer(answer: answerUpdated, answerContent: self.answer, idCategory: idCategory){
+                                        self.isPresented.toggle()
                                     }
-                                    guard let nbLike = AnswerQueryService().getNbLikeAnswer(idAnswer: idAnswerCreated) else{
-                                     return
-                                    }
-                                    let newAnswer = Answer(idAnswer: idAnswerCreated, answer: self.answer, category: idCategory, user: user,nbLike: nbLike)
-                                    remark.answerSet.addAnswers(answer: newAnswer)
-                                    self.isPresented.toggle()
                                 }
                             }
                             
+                        }else{
+                            self.showingAlert = true
                         }
-                    }else{
-                        
-                        if let answerUpdated = self.answerUpdated{
-                            if AnswerQueryService().updateAnswer(answer: answerUpdated, answerContent: self.answer, idCategory: idCategory){
-                                self.isPresented.toggle()
-                            }
-                        }
+                    })
+                    {
+                        Text("Send").font(.title).padding(10).foregroundColor(.purple).background(Color.white)
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 3)
+                    }.alert(isPresented: $showingAlert) {
+                        Alert(title: Text("Empty Field"), message: Text("Please fill the fields"), dismissButton: .default(Text("Got it !")))
                     }
-                    
-                }else{
-                    self.showingAlert = true
                 }
-            })
-            {
-                Text("Send").font(.title).padding(10).foregroundColor(.purple).background(Color.white)
-                .cornerRadius(20)
-                .shadow(color: .gray, radius: 3)
-            }.alert(isPresented: $showingAlert) {
-                Alert(title: Text("Empty Field"), message: Text("Please fill the fields"), dismissButton: .default(Text("Got it !")))
+                
+                
             }
             
-        }.padding(10)
+        }
         
     }
 }
 
 /*struct CreateAnswerView_Previews: PreviewProvider {
-    static var previews: some View {
-        CreateAnswerView(isPresented: <#Binding<Bool>#>)
-    }
-}*/
+ static var previews: some View {
+ CreateAnswerView(isPresented: <#Binding<Bool>#>)
+ }
+ }*/
--- PEPSProject/RemarksViews/CreateRemarkView.swift
+++ PEPSProject/RemarksViews/CreateRemarkView.swift
@@ -22,70 +22,90 @@ struct CreateRemarkView: View {
     @State var errorMessage : String = ""
     
     var body: some View {
-        VStack{
-            if(!isUpdateView){
-                Text("Create a new Remark").font(.title).foregroundColor(.purple)
-                .cornerRadius(10.0).padding()
-            }else{
-                Text("Update your Remark").font(.title).foregroundColor(.purple)
-                .cornerRadius(10.0).padding()
-            }
-            
-            TextField("Post your remark...", text: $remark).padding().background(Color(.secondarySystemBackground)).cornerRadius(10)
-            
-            Picker(selection: $selectedidCategory, label: Text("Category")){
-                ForEach(0 ..< categorySet.categorySet.count) {
-                    Text("\(self.categorySet.categorySet[$0].lib)").padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground)).cornerRadius(10)
-                }
-            }.padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground))
-            .cornerRadius(10)
-            
-            if(!isUpdateView){
-                TextField("Location...", text: $location).padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground)).cornerRadius(10)
-            }
+        
+        NavigationView{
             
             
-            Button(action:{
-                if self.remark != "" && self.location != ""{
-                    let idCategory = self.categorySet.categorySet[self.selectedidCategory].idCategory
-                    if(!self.isUpdateView){
-                        if let idRemarkCreated = RemarkQueryService().createRemark(remark: self.remark, idCategory: idCategory, location: self.location){
-                            guard let user = UserQueryService().getUserLogged() else{
-                             return
-                            }
-                            let newRemark = Remark(idRemark: idRemarkCreated, remark: self.remark, idCategory: self.selectedidCategory, user: user, location: self.location)
-                            self.remarkSet.addRemarks(remark: newRemark)
-                            self.isPresented.toggle()
-                        }else{
-                            self.errorMessage = "Create failed, please try again."
-                            self.showingAlert = true
-                        }
-                    }else{
-                        if let remarkUpdated = self.remarkUpdated{
-                            if RemarkQueryService().updateRemark(remark: remarkUpdated, remarkContent: self.remark, idCategory: idCategory){
-                                self.isPresented.toggle()
+            VStack{
+                Form{
+                    
+                    Section{
+                        
+                        VStack{
+                            
+                            if(!isUpdateView){
+                                Text("Create a new Remark").font(.title).foregroundColor(.purple)
+                                    .cornerRadius(10.0).padding()
                             }else{
-                                self.errorMessage = "Update failed, please try again."
-                                self.showingAlert = true
+                                Text("Update your Remark").font(.title).foregroundColor(.purple)
+                                    .cornerRadius(10.0).padding()
                             }
+                            
+                            TextField("Post your remark...", text: $remark).padding().background(Color(.secondarySystemBackground)).cornerRadius(10)
+                            
+                            Picker(selection: $selectedidCategory, label: Text("Category")){
+                                ForEach(0 ..< categorySet.categorySet.count) {
+                                    Text("\(self.categorySet.categorySet[$0].lib)").padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground)).cornerRadius(10)
+                                }
+                            }.padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground))
+                                .cornerRadius(10)
+                            
+                            if(!isUpdateView){
+                                TextField("Location...", text: $location).padding().foregroundColor(.secondary)  .background(Color(.secondarySystemBackground)).cornerRadius(10)
+                            }
+                            
                         }
                         
                     }
                     
-                }else{
-                    self.errorMessage = "Please fill the fields"
-                    self.showingAlert = true
                 }
-            })
-            {
-                Text("Send").font(.title).padding(10).foregroundColor(.purple).background(Color.white)
-                    .cornerRadius(20)
-                    .shadow(color: .gray, radius: 3)
-            }.alert(isPresented: $showingAlert) {
-                Alert(title: Text("A problem occured"), message: Text("\(self.errorMessage)"), dismissButton: .default(Text("Got it !")))
+                VStack{
+                    
+                    Button(action:{
+                        if self.remark != "" && self.location != ""{
+                            let idCategory = self.categorySet.categorySet[self.selectedidCategory].idCategory
+                            if(!self.isUpdateView){
+                                if let idRemarkCreated = RemarkQueryService().createRemark(remark: self.remark, idCategory: idCategory, location: self.location){
+                                    guard let user = UserQueryService().getUserLogged() else{
+                                        return
+                                    }
+                                    let newRemark = Remark(idRemark: idRemarkCreated, remark: self.remark, idCategory: self.selectedidCategory, user: user, location: self.location)
+                                    self.remarkSet.addRemarks(remark: newRemark)
+                                    self.isPresented.toggle()
+                                }else{
+                                    self.errorMessage = "Create failed, please try again."
+                                    self.showingAlert = true
+                                }
+                            }else{
+                                if let remarkUpdated = self.remarkUpdated{
+                                    if RemarkQueryService().updateRemark(remark: remarkUpdated, remarkContent: self.remark, idCategory: idCategory){
+                                        self.isPresented.toggle()
+                                    }else{
+                                        self.errorMessage = "Update failed, please try again."
+                                        self.showingAlert = true
+                                    }
+                                }
+                                
+                            }
+                            
+                        }else{
+                            self.errorMessage = "Please fill the fields"
+                            self.showingAlert = true
+                        }
+                    })
+                    {
+                        Text("Send").font(.title).padding(10).foregroundColor(.purple).background(Color.white)
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 3)
+                    }.alert(isPresented: $showingAlert) {
+                        Alert(title: Text("A problem occured"), message: Text("\(self.errorMessage)"), dismissButton: .default(Text("Got it !")))
+                    }
+                }
+                
+                
             }
             
-        }.padding(10)
+        }
         
     }
 }
